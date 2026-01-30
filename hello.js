14363dc0597d1bbc47a54aa4e9455f195a7ab2a4
saivarsha1008 <245123737122@mvsrec.edu.in>
2026-01-30 10:16:06 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,11 @@
+<html>
+  <head>
+  <title>hello</title>
+  </head>
+<body>
+  <p id="demo"></p>
+  <script>
+  document.getElementById("demo").textContent="hello world:";
+</script>
+</body>
+</html>
